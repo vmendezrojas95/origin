dcae1c30275d0deaa7f81dfd069076aff80468cc
vmendezrojas <vmendezrojas95@gmail.com>
2023-02-07 20:43:59 -0300

    levantando web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedSocialComponent } from './components/red-social/red-social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { EduComponent } from './components/edu/edu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedSocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExpComponent,
+    EduComponent
   ],
   imports: [
     BrowserModule,
